d9c72de6b26ff4b47c90c337d52f1abb16cf27df
Briandvlpr <brianlagranda@gmail.com>
2024-01-18 17:30:47 -0300

    feat: Destructuring props, added some stylish and description to each movie

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import noImagePlaceholder from '../assets/noImagePlaceholder.png';
+import { truncateText } from '../utils/functions.tsx';
 
 interface Movie {
     id: number;
     title: string;
     poster_path: string;
+    overview: string;
 }
 
 interface SearchResultsProps {
@@ -22,9 +24,9 @@ const SearchResults: React.FC<SearchResultsProps> = ({ results }) => {
                 results[0].title === '' ? '' : 'bg-zinc-800'
             } -mt-3 p-3 rounded-b-md`}
         >
-            {results.map((result, index) => (
-                <li key={index} className="mb-4 mt-2 flex p-2 gap-3">
-                    {result.poster_path === null ? (
+            {results.map(({ poster_path, title, overview }, index) => (
+                <li key={index} className="mt-2 flex p-2 gap-3">
+                    {poster_path === null ? (
                         <img
                             className="w-1/3 md:w-1/5 rounded-md object-contain"
                             src={noImagePlaceholder}
@@ -32,10 +34,13 @@ const SearchResults: React.FC<SearchResultsProps> = ({ results }) => {
                     ) : (
                         <img
                             className="w-1/3 md:w-1/5 rounded-md object-contain"
-                            src={`https://image.tmdb.org/t/p/w500/${result.poster_path}`}
+                            src={`https://image.tmdb.org/t/p/w500/${poster_path}`}
                         ></img>
                     )}
-                    {result.title}
+                    <div className="w-full p-2 border border-gray-200/20 rounded-md bg-gray-200/10">
+                        <h2 className="text-center">{title}</h2>
+                        <p className="p-4">{truncateText(overview)}</p>
+                    </div>
                 </li>
             ))}
         </ul>
