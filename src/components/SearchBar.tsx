36b082c1a44d0c8bee6cd2ffdbd0e62c21ee27ce
Briandvlpr <brianlagranda@gmail.com>
2024-01-18 17:28:59 -0300

    Chore: Deleted unused button for Search. Actually searching is based on every keystroke

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -3,15 +3,12 @@ import SearchResults from './SearchResults';
 import useMovieSearch from '../hooks/useMovieSearch';
 
 const SearchBar: React.FC = () => {
-    const { searchTerm, setSearchTerm, movies, loading, error, searchMovies } =
+    const { searchTerm, setSearchTerm, movies, loading, error } =
         useMovieSearch();
 
     return (
         <div className="flex flex-col items-center mt-8">
             <div className="gap-2 w-full md:w-3/4 md:grid-cols-[10fr,1fr] lg:w-2/4">
-                <button onClick={searchMovies} disabled={loading}>
-                    Search
-                </button>
                 <input
                     className="w-full h-10 rounded-md p-3 bg-zinc-800 focus:outline-none focus:ring-0"
                     type="text"
